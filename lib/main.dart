bfdda3942aa6e7bcf2482daefb5b4e73b0835e8d
Dominik Schneider <Dominik.Schneider@dm.de>
2020-08-14 13:44:23 +0200

    first dayview mockup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+import 'package:therapy_calendar/model/batch.dart';
+import 'package:therapy_calendar/model/dose.dart';
+import 'package:therapy_calendar/model/medication_entry.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,35 +14,44 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: DayView(),
-      );
+  Widget build(BuildContext context) {
+    Intl.defaultLocale = 'de_DE';
+    initializeDateFormatting();
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: DayView(),
+    );
+  }
 }
 
 class DataClass {
+  DataClass(this.date, this.batch, this.dose);
+
   final DateTime date;
   final String batch;
   final String dose;
-
-  DataClass(this.date, this.batch, this.dose);
 }
 
 class DayView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final def = DataClass(DateTime.now(), 'batch', 'dose');
-    final defaultList = <DataClass>[];
+    final def = MedicationEntry((medication) => medication
+      ..batch = Batch((batch) => batch.number = 2).toBuilder()
+      ..date = DateTime.now()
+      ..dose = Dose((dose) => dose
+        ..count = 2
+        ..singleDose = 20).toBuilder());
+    final defaultList = <MedicationEntry>[];
+    final rng = Random();
     for (var i = 0; i < 100; i++) {
-      defaultList.add(def);
+      defaultList.add(def.rebuild(
+          (medication) => medication.batch.number = rng.nextInt(210243)));
     }
-    Intl.defaultLocale = 'de_DE';
-    initializeDateFormatting();
-
     return Scaffold(
       appBar: AppBar(title: const Text('Therapy Calendar')),
       body: Column(
@@ -63,9 +77,9 @@ class DayView extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text('$date $index'),
-                      Text('${item.batch} $index'),
-                      Text('${item.dose} $index'),
+                      Text('$date'),
+                      Text('${item.batch.number}'),
+                      Text('${item.dose.count} x ${item.dose.singleDose} ml'),
                     ],
                   ),
                 );
