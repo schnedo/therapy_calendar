40ded7fb0b173b5421d2b7574a29aa5faa6fc11b
Moritz Sauter <sauter@synyx.de>
2020-08-21 16:51:29 +0200

    Used new MedicationEntry-Card

--- lib/views/day_view.dart
+++ lib/views/day_view.dart
@@ -1,11 +1,36 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 import 'package:therapy_calendar/bloc/medication_entry_bloc.dart';
 import 'package:therapy_calendar/generated/l10n.dart';
+import 'package:therapy_calendar/model/batch_number.dart';
+import 'package:therapy_calendar/model/dose.dart';
+import 'package:therapy_calendar/model/medicament.dart';
+import 'package:therapy_calendar/model/medication.dart';
 import 'package:therapy_calendar/model/medication_entry.dart';
-import 'package:therapy_calendar/widgets/medication_entry/add.dart';
+import 'package:therapy_calendar/widgets/medication_entry/card.dart';
+
+MedicationEntry _someEntry() => MedicationEntry((medication) => medication
+  ..medications = ListBuilder<Medication>([
+    Medication(
+      (b) => b
+        ..medicament = (MedicamentBuilder()
+          ..batchNumber = (BatchNumberBuilder()..number = 12345678)
+          ..name = 'Medikament')
+        ..dose = (DoseBuilder()..amount = 20),
+    ),
+    Medication(
+      (b) => b
+        ..medicament = (MedicamentBuilder()
+          ..batchNumber = (BatchNumberBuilder()..number = 12345678)
+          ..name = 'Medikament')
+        ..dose = (DoseBuilder()..amount = 20),
+    )
+  ])
+  ..date = DateTime.now()
+  ..duration = const Duration(hours: 2)
+  ..comments = 'Einnahme 400 mg Paracetamol');
 
 class DayView extends StatelessWidget {
   static const routeName = '/';
@@ -15,7 +40,8 @@ class DayView extends StatelessWidget {
         appBar: AppBar(title: Text(S.of(context).dayViewTitle)),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Navigator.pushNamed(context, AddMedicationEntry.routeName);
+            context.bloc<MedicationEntryBloc>().add(_someEntry());
+//            Navigator.pushNamed(context, AddMedicationEntry.routeName);
           },
           child: const Icon(Icons.add),
         ),
@@ -23,35 +49,20 @@ class DayView extends StatelessWidget {
           children: [
             ListTile(
                 title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text('DATUM'),
-                const Text('BATCH'),
-                const Text('DOSIS'),
+                Text(S.of(context).dayViewDateHeader),
+                Text(S.of(context).dayViewMedicationHeader),
+                Text(S.of(context).dayViewDurationHeader),
               ],
             )),
+            const Divider(),
             Expanded(
                 child: BlocBuilder<MedicationEntryBloc, List<MedicationEntry>>(
               buildWhen: (_, __) => true,
               builder: (context, medicationEntries) => ListView.builder(
-                itemBuilder: (ctx, index) {
-                  final item = medicationEntries[index];
-                  final date = DateFormat.E().add_d().format(item.date);
-
-                  return Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('$date'),
-                        Text('${item.medications}'),
-//                        Text('${item.dose.count} x'
-//                            '${item.dose.singleDose} ml'),
-                      ],
-                    ),
-                  );
-                },
+                itemBuilder: (ctx, index) =>
+                    MedicationEntryCard(entry: medicationEntries[index]),
                 itemCount: medicationEntries.length,
               ),
             )),
