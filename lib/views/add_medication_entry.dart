d990321e0b189a6bdccf40543720dd30137c2a07
Moritz Sauter <sauter@synyx.de>
2020-09-04 11:45:20 +0200

    Added FormFields for the model
    THese work as normal form fields do, with eigher an onSaved or an
    onChanged action. Additionally they use themselves when needed for the
    construction of the value.

--- lib/views/add_medication_entry.dart
+++ lib/views/add_medication_entry.dart
@@ -1,89 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:therapy_calendar/bloc/medication_entry_bloc.dart';
-import 'package:therapy_calendar/model/medication_entry.dart';
+import 'package:therapy_calendar/generated/l10n.dart';
+import 'package:therapy_calendar/widgets/medication_entry/add.dart';
 
 class AddMedicationEntry extends StatelessWidget {
   static const routeName = '/medication_entry/add';
 
   final _formKey = GlobalKey<FormState>();
-  final _builder = MedicationEntryBuilder();
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: const Text('Neuer Eintrag'),
+          title: Text(S.of(context).addMedicationEntryViewTitle),
         ),
         body: Form(
           key: _formKey,
           child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              children: [
-                RaisedButton(
-                  onPressed: () => showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    lastDate: DateTime.now(),
-                    firstDate: DateTime(2020),
-                  ).then((value) => _builder.date = value),
-                  child: const Text('Datum eingeben'),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: 'Medikamentenbezeichnung'),
-                  validator: (value) => value.isEmpty
-                      ? 'Bitte Medikamentenbezeichnung eingeben'
-                      : null,
-                  onSaved: (value) {
-                    //_builder.medication.name = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Chargennummer'),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  validator: (value) =>
-                      value.isEmpty ? 'Bitte Chargennummer eingeben' : null,
-                  onSaved: (value) {
-                    // _builder.medication.batchNumber = int.parse(value);
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Einzeldosis'),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  validator: (value) =>
-                      value.isEmpty ? 'Bitte Einzeldosis eingeben' : null,
-                  onSaved: (value) {
-                    //_builder.dose.singleDose = int.parse(value);
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Anzahl Dosen'),
-                  initialValue: '1',
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  validator: (value) =>
-                      value.isEmpty ? 'Bitte Anzahl Dosen eingeben' : null,
-                  onSaved: (value) {
-                    //_builder.dose.count = int.parse(value);
-                  },
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      _formKey.currentState.save();
-                      context.bloc<MedicationEntryBloc>().add(_builder.build());
-                      Navigator.pop(context);
-                    }
-                  },
-                  child: const Text('speichern'),
-                )
-              ],
-            ),
-          ),
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  AddMedicationEntryFormField(
+                    onSaved: (entry) {
+                      context.bloc<MedicationEntryBloc>().add(entry);
+                    },
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        Navigator.pop(context);
+                      }
+                    },
+                    child:
+                        Text(S.of(context).addMedicationEntryViewSubmitButton),
+                  )
+                ],
+              )),
         ),
       );
 }
