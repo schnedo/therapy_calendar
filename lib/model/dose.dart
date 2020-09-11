61d1c5954e69890b93e3d16f66e1dfff8ae53de3
Moritz Sauter <sautermoritz@gmx.de>
2020-09-11 14:03:54 +0200

    Add unit to dose form

--- lib/model/dose.dart
+++ lib/model/dose.dart
@@ -7,10 +7,12 @@ abstract class Dose implements Built<Dose, DoseBuilder> {
   factory Dose([void Function(DoseBuilder) updates]) = _$Dose;
   Dose._();
 
+  static const unit = 'ml';
+
   int get amount;
 
   @override
-  String toString() => '$amount ml';
+  String toString() => '$amount $unit';
 
   static Serializer<Dose> get serializer => _$doseSerializer;
 }
